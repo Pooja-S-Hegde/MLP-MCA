7ec20811c522611b8c2628a293470071d6f14148
Pooja-S-Hegde <hegdepooja555@gmail.com>
2025-05-11 09:41:27 +0530

    Program 1

---
+++ program1.py
@@ -0,0 +1,16 @@
+wheels=int(input("Enter no of wheels:"))
+
+if wheels==2:
+    print("It's likely a bike or scooter")
+elif wheels==3:
+    print("It's likely an Autorikshaw or tricycle")
+elif wheels==4:
+    print("It's likely a car or van")
+elif wheels==5:
+    print("It's likely a mini truck or delivery van")
+elif wheels==8:
+    print("It's likely a large truck")
+elif wheels>8:
+    print("It's likely a heavy duty vehicle like trailor")
+else:
+    print("Unknown vehicle type")
