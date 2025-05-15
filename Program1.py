5ac41fb1c4801648f1871eb38c7ac4da8e88ee5f
sheetala0 <sheetalahegde39@gmail.com>
2025-05-15 15:17:13 +0530

    MLP first Program

---
+++ Program1.py
@@ -0,0 +1,28 @@
+import pandas as pd
+data=pd.read_csv("C:/BostonHousing_pgm1.csv")
+print(data.head())
+print(data.tail())
+print(data.isnull().sum())
+X=data[['rm']] #
+y=data['medv'] #median values
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y)
+from sklearn.linear_model import LinearRegression
+model=LinearRegression()
+model.fit(X_train,y_train)
+#predict
+y_pred=model.predict(X_test)
+
+from sklearn.metrics import mean_squared_error,r2_score
+
+#evaluate
+print("R^2:",r2_score(y_test,y_pred))
+print("MSE:",mean_squared_error(y_test,y_pred))
+import matplotlib.pyplot as plt
+plt.scatter(X_test,y_test,color='blue',label='Actual')
+plt.plot(X_test,y_pred,color='red',linewidth=2,label='Predicted')
+plt.xlabel("Rooms per House")
+plt.ylabel("  House Price")
+plt.title("Linear Regression")
+plt.legend()
+plt.show()
